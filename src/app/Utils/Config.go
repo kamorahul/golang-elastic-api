99c09d251017cf4f77a73b88eead4c9241a6eee1
Rahul Kamboj <kamo.rahul@gmail.com>
2016-12-07 18:16:46 +0530

    Update Config.go

--- src/app/Utils/Config.go
+++ src/app/Utils/Config.go
@@ -4,7 +4,7 @@ func ElasticUrl() string{
 
 	str := GetRecordSrv("elastic.service.consul")
 	if str == ""{
-		return "http://192.168.86.103:9200"
+		return "http://127.0.0.1:9200"
 	}
 	return str;
 
